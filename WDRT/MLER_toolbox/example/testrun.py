a921c35cee990d29757011df99c5b074af67be65
Eliot Quon <eliot.quon@nrel.gov>
2016-09-02 11:28:21 -0600

    Cleaned up files a bit.

--- WDRT/MLER_toolbox/example/testrun.py
+++ WDRT/MLER_toolbox/example/testrun.py
@@ -1,13 +1,9 @@
-#!/usr/bin/python
-
-libpath     = '/Users/equon/WDRT/WDRT/MLER_toolbox'
+import sys, os
+libpath     = os.environ['HOME']+'/WDRT/WDRT/MLER_toolbox'
 RAOdir      = libpath+'/example/RAO_data/'
 outputDir   = libpath+'/example/TestData/'
-
-import sys
 if (libpath not in sys.path): sys.path.append(libpath)
 import mler
-
 
 # Create the object
 Test = mler.mler(H=9.0, T=15.1, numFreq=500)
@@ -26,12 +22,9 @@ Test.readRAO(5,RAOdir+'RAO_pitch_RM3float.dat')
 Test.plotRAO(3,show=False)
 Test.plotRAO(5)
 
-#
-# heave conditioned response
-#
-
 # Now that everything is set up, generate the MLER wave for heave.
-Test.MLERcoeffsGen(3,1.0)   # generate the wave profile, 1 meter heave response desired
+Test.MLERcoeffsGen(3,1.0)   # generate the heave-conditioned wave profile, 1 meter heave response desired
+#Test.MLERcoeffsGen(5,1.0)   # generate the pitch-conditioned wave profile, 1 meter heave response desired
 
 # At this point we can export the coefficients.  The coefficients will
 # match a desired response height that was given as the second argument to
@@ -52,19 +45,4 @@ Test.MLERexportWECSim(outputDir+'Test_MLER_heaveOpt_WECSimInput.txt')
 #Test.MLERanimate(3,export='Movie_Heave')        # export without plotting
 Test.MLERanimate(3)                             # plot animation
 Test.MLERexportMovie(outputDir+'Movie_Heave')   # export generated animation (after plotting)
-
-# make a movie of the sequence for pitch
-#Test.MLERanimate(5,export=outputDir+'Movie_Pitch') # export without plotting
-
-# make a movie of the combined heave + pitch response
-#Test.MLERanimate2D(export=outputDir+'Movie_2D_heaveOpt')
-
-
-#
-# pitch conditioned response
-#
-
-#Test.MLERcoeffsGen(5,1.0)   # generate the wave profile, 1 meter heave response desired
-#Test.MLERwaveAmpNormalize(Test.waves.H/2 * 1.9) # the desired peak height (peak to MSL)
-#Test.MLERanimate2D(export=outputDir+'Movie_2D_pitchOpt')
 
