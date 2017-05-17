b79a54ac65e0d6b39947c38d25a16cbf09f417dd
Esterly <trester@srn.sandia.gov>
2017-05-17 16:24:10 -0600

    Updating setup.py file

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-
-
+#Preliminary setup.py file for WDRT
 setup(
     name = "WDRT",
     version = "1.0.0",
