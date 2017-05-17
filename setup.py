b04e512b708eeb4970b92961ee52da0333315e1c
Esterly <trester@srn.sandia.gov>
2017-05-17 16:29:25 -0600

    Adding dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     name = "WDRT",
     version = "1.0.0",
     url = "https://github.com/WEC-Sim/WDRT",
+    install_requires=['numpy', 'scipy', 'requests', 'bs4', 'sklearn','matplotlib'],
     packages=['WDRT', 'examples'],
 )
