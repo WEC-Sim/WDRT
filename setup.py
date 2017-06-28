cfcb74ac8a40225b988bd049464a54227b1cc2f0
Tyler <tyleresterly@unm.edu>
2017-06-28 16:11:27 -0600

    Updating Setup File

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     name = "WDRT",
     version = "1.0.0",
     url = "https://github.com/WEC-Sim/WDRT",
-    install_requires=['numpy', 'scipy', 'requests', 'bs4', 'sklearn','matplotlib'],
+    install_requires=['numpy', 'scipy', 'requests', 'bs4', 'sklearn','matplotlib','netCDF4'],
     packages=['WDRT', 'examples'],
 )
