d35cbfd9427623bf6643652ff9dbc5d7a210d659
ssolson <ssolson@users.noreply.github.com>
2020-05-13 11:08:47 -0600

    add h5py to the required packages

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     name = "WDRT",
     version = "1.0.0",
     url = "https://github.com/WEC-Sim/WDRT",
-    install_requires=['numpy', 'scipy', 'requests', 'bs4', 'sklearn','matplotlib','netCDF4', 'statsmodels', 'lxml'],
+    install_requires=['numpy', 'scipy', 'requests', 'bs4', 'sklearn','matplotlib','netCDF4', 'statsmodels', 'lxml', 'h5py'],
     packages=['WDRT', 'examples', 'WDRT.mler'],
 )
